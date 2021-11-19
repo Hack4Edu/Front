206fb2678212404abd078b318f194fe3e0816407
Lucas Borges <lucas_borges_br@hotmail.com>
2021-11-19 15:04:46 -0300

    ajuste videos

--- src/Pages/Upload/index.js
+++ src/Pages/Upload/index.js
@@ -1,10 +1,13 @@
 import React, { Fragment } from "react";
+import './style.css'
 import Header from "../../Components/Header";
 
 export default function Upload() {
   return (
     <Fragment>
+      
       <Header />
+      <div className="container">
       <div>
         <h1>Repositório de videos</h1>
       </div>
@@ -12,8 +15,13 @@ export default function Upload() {
       <div>
         <h1>Aprenda LIBRAS com eficiência e rapidez (vocabulários básicos)</h1>
       </div>
-    <iframe width="560" height="315" src="https://www.youtube.com/embed/mBeLLxSUW9k" title="YouTube video player" frameborder="0" allow="accelerometer; autoplay; clipboard-write; encrypted-media; gyroscope; picture-in-picture" allowfullscreen></iframe>
+    <iframe width="320" height="240" src="https://www.youtube.com/embed/mBeLLxSUW9k" title="YouTube video player" frameborder="0" allow="accelerometer; autoplay; clipboard-write; encrypted-media; gyroscope; picture-in-picture" allowfullscreen></iframe>
     </div>
+    <div>
+    <iframe width="320" height="240" src="https://www.youtube.com/embed/H-1zwazpt58" title="YouTube video player" frameborder="0" allow="accelerometer; autoplay; clipboard-write; encrypted-media; gyroscope; picture-in-picture" allowfullscreen></iframe>
+    </div>
+    </div>
+    
 
     </Fragment>
   );

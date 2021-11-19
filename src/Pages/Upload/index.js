f375a8a856553ff154b25848520a84afe88c1c79
Lucas Borges <lucas_borges_br@hotmail.com>
2021-11-19 16:33:35 -0300

    ajustes no desk

--- src/Pages/Upload/index.js
+++ src/Pages/Upload/index.js
@@ -8,20 +8,23 @@ export default function Upload() {
       <Header />
       <div className="container">
         <div>
-          <h1>Repositório de videos</h1>
+          <h1>Turma: Tecnologia de sistemas para internet</h1>
+        </div>
+        <div>
+          <h2>Repositório de videos</h2>
         </div>
         <div>
           <div>
-            <h1>
-              Aprenda LIBRAS com eficiência e rapidez (vocabulários básicos)
-            </h1>
+            <h4>
+              Disciplina: Administração de Servidores
+            </h4>
+            <h5>Tradutora: Daiane</h5>
           </div>
         </div>
 
         <div>
           <iframe
-            width="320"
-            height="240"
+           width="560" height="315"
             src="https://www.youtube.com/embed/WC_j3LeIVko"
             title="YouTube video player"
             frameborder="0"
@@ -31,8 +34,7 @@ export default function Upload() {
         </div>
         <div>
           <iframe
-            width="320"
-            height="240"
+            width="560" height="315"
             src="https://www.youtube.com/embed/H-1zwazpt58"
             title="YouTube video player"
             frameborder="0"
@@ -43,8 +45,7 @@ export default function Upload() {
 
         <div>
           <iframe
-            width="320"
-            height="240"
+            width="560" height="315"
             src="https://www.youtube.com/embed/PXCEjl4GX88"
             title="YouTube video player"
             frameborder="0"
@@ -55,8 +56,7 @@ export default function Upload() {
 
         <div>
           <iframe
-            width="320"
-            height="240"
+            width="560" height="315"
             src="https://www.youtube.com/embed/P0XPZULbVt0"
             title="YouTube video player"
             frameborder="0"
@@ -66,8 +66,7 @@ export default function Upload() {
         </div>
         <div>
           <iframe
-            width="320"
-            height="240"
+            width="560" height="315"
             src="https://www.youtube.com/embed/mBeLLxSUW9k"
             title="YouTube video player"
             frameborder="0"
